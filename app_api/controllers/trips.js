15f3735b919302b1814ae433b97ae8f8de874ce7
Anthony M <minunnia@gmail.com>
2022-02-13 00:52:19 -0500

    SPA

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,4 +1,6 @@
+const { model } = require('mongoose');
 const mongoose = require('mongoose'); //.set('debug', true);
+const { param } = require('../../app_server/routes');
 const Model = mongoose.model('trips');
 
 // GET: /trips - list all the trips
@@ -43,7 +45,69 @@ const tripsFindCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    model
+    .create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    },
+    (err, trip) => {
+        if (err) {
+            return res
+                .status(400) // bad rtequest, invalid content
+                .json(err);
+        } else {
+            return res
+                .status(201) // created
+                .json(trip)
+        }
+    });
+}
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    model
+        .findOneandUpdate({ 'code': req.params.tripCode }, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, { new: true })
+        .then(trip => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+}
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
